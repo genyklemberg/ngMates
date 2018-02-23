5c2f019944fb438a10f16246ba5f24d7b05d60e7
Yevheniy Potupa <y.potupa@gmail.com>
2018-02-23 14:56:48 +0200

    added mailgun email in functions

--- functions/index.js
+++ functions/index.js
@@ -1,76 +1,33 @@
-// const functions = require('firebase-functions');
-// const express = require('express');
-// const fetch = require('node-fetch');
-// const url = require('url');
-//
-// const app = express();
-//
-// const appUrl = 'https://ngmates.com';
-// const renderUrl = 'https://render-tron.appspot.com/render';
-//
-// function generateUrl(request) {
-//   return url.format({
-//     protocol: request.protocol,
-//     host: appUrl,
-//     pathname: request.originalUrl
-//   });
-// }
-//
-// function detectBot(userAgent) {
-//   const bots = [
-//     'googlebot',
-//     'bingbot',
-//     'yandexbot',
-//     'duckduckbot',
-//     'slurp',
-//     'twitterbot',
-//     'facebookexternalhit',
-//     'linkedinbot',
-//     'embedly',
-//     'baiduspider',
-//     'slackbot',
-//     'vkShare',
-//     'facebot',
-//     'outbrain',
-//     'W3C_Validator'
-//   ]
-//   const agent = userAgent.toLowerCase();
-//
-//   for(const bot of bots) {
-//     if(agent.indexOf(bot) > -1) {
-//       console.log('bot');
-//       return true
-//     }
-//     console.log('not bot');
-//     return false
-//   }
-//
-// }
-//
-//
-//
-// app.get('*', (req,res) => {
-//   const isBot = detectBot(req.headers['user-agent']);
-//
-//   if (isBot) {
-//     const botUrl = generateUrl(req);
-//
-//     fetch(`${renderUrl}/${botUrl}`)
-//       .then(res => res.next())
-//       .then(body => {
-//         res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-//         res.set('Vary', 'User-Agent');
-//
-//         res.send(body.toString())
-//     });
-//   } else {
-//     fetch(`https://${appUrl}`)
-//       .then(res => res.text())
-//       .then(body => {
-//         res.send(body.toString());
-//     });
-//   }
-// });
-//
-// exports.app = functions.https.onRequest(app);
+const functions = require('firebase-functions');
+const cors = require('cors');
+
+var api_key = 'key-685c46166f0607fbf761f2ce8b4329a0';
+// var domain = 'flyhigh-5416b.firebaseapp.com';
+var domain = 'sandbox4f0d1573738a4c02816238a129149267.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+function mailgunMessage(req, res) {
+  var data = {
+    from: req.body.from,
+    to: 'aputop@yahoo.com',
+    subject: 'Flyhigh запрос',
+    // subject: req.body.subject,
+    // text: req.body.text
+    html: '<h1>Меня звать: '+ req.body.name + '</h1><br><p>email: ' + req.body.from +
+    '</p><br><p>тема запроса: ' + req.body.subject +
+    '</p><br><p>мне нужно: ' + req.body.text + '</p>'
+  };
+  mailgun.messages().send(data).then(function(){
+    res.status(200).json({success:true});
+  }).catch(function() {
+    res.status(500).json({success:false});
+  });
+}
+
+exports.httpEmail = functions.https.onRequest(function(req, res) {
+  const corsFn = cors();
+  corsFn(req, res, function() {
+    mailgunMessage(req, res);
+  });
+});
 
